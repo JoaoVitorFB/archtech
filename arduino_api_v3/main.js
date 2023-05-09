91239737ea47430bed35f4b585f77c55fb7e137e
JoaoVitorFB <joao.batista@sptech.school>
2023-05-09 12:20:09 -0300

    funfando banco so na pasta arduino

--- arduino_api_v3/main.js
+++ arduino_api_v3/main.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 
 const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3000;
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 const serial = async (
     valoresDht11Umidade,
@@ -17,8 +17,8 @@ const serial = async (
         {
             host: 'localhost',
             port: 3306,
-            user: 'root',
-            password: 'urubu100',
+            user: 'aluno',
+            password: 'sptech',
             database: 'metricas'
         }
     ).promise();

b78d413fd3578569d62f4cb7e511a7f521cf4515
gilj011 <gilberto.camposjr@sptech.school>
2023-05-09 14:26:54 -0300

    faltando ajustes no tempo de captura do sensor

--- arduino/joao_arduino/joao_arduino.ino
+++ arduino/joao_arduino/joao_arduino.ino
@@ -10,17 +10,22 @@ void setup() {
 }
 int pessoa = 0;
 int tempo = 0;
+int detect = 0;
 void loop() {
 
-  for (int i = 0; i < 5000; i = i + 1000) {
+  for (int i = 0; i < 10000; i = i + 500) {
     int chave = digitalRead(7);
-    if (chave == 0) {
+    if (chave == 0 && detect == 0) {
+      detect++;
       pessoa++;
+    } else if (chave == 1 && detect != 0){
+      detect = 0;
     }
-    delay(1000);
+    delay(500);
   }
 
 Serial.println(pessoa);
 pessoa = 0;
+detect = 0;
 
 }
